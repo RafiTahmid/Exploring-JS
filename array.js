dbd3583f9ecf5fbf9f2b584be361c758204d6991
RafiTahmid <rafi.tahmid019@gmail.com>
2022-08-01 15:41:56 +0600

    array index

--- array.js
+++ array.js
@@ -1,9 +1,21 @@
 var sampleArray = [5, 20, 10, 15, 19, 9];
 var sampleArray2 = ["John", 'Lisa', "Addams", 'Texas'];
 
-console.log(sampleArray)
-console.log(sampleArray2)
-console.log(sampleArray. length)
-console.log(sampleArray2. length)
+// console.log(sampleArray)
+// console.log(sampleArray2)
+// console.log(sampleArray. length)
+// console.log(sampleArray2. length)
 
-console.log(sampleArray + " " +sampleArray2);
+// console.log(sampleArray + " " +sampleArray2);
+
+var element = sampleArray2[3];
+
+// console.log(sampleArray[1]);
+// console.log(element);
+
+sampleArray2[4] = 'Kendra';
+
+// console.log(sampleArray2[4]);
+
+var positionIndex = sampleArray.indexOf(99);
+console.log(positionIndex);
