4ddf458f329909ca983ca112c50ee9a457431acd
RafiTahmid <rafi.tahmid019@gmail.com>
2022-08-01 11:23:44 +0600

    intro

---
+++ array.js
@@ -0,0 +1,9 @@
+var sampleArray = [5, 20, 10, 15, 19, 9];
+var sampleArray2 = ["John", 'Lisa', "Addams", 'Texas'];
+
+console.log(sampleArray)
+console.log(sampleArray2)
+console.log(sampleArray. length)
+console.log(sampleArray2. length)
+
+console.log(sampleArray + " " +sampleArray2);
