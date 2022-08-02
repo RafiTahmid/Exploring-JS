87057f3792d8a8f1c3f4eb78e3e4c968e377b359
RafiTahmid <rafi.tahmid019@gmail.com>
2022-08-02 18:19:35 +0600

    more simple things

---
+++ array-loop.js
@@ -0,0 +1,5 @@
+var numbers = [45, 67, 89, 99, 12, 35]
+for (var i = 0; i< numbers.length; i++){
+    var number = numbers[i]
+    console.log(number)
+}
--- while.js
+++ while.js
@@ -1,6 +1,13 @@
-var kacchiGiven = 0;
+// var kacchiGiven = 0;
 
-while(kacchiGiven < 5){
-    console.log('Kacchi khabo..Kacchii  !!')
-    kacchiGiven ++;
+// while(kacchiGiven < 5){
+//     console.log('Kacchi khabo..Kacchii  !!')
+//     kacchiGiven ++;
+// }
+
+var number = 0;
+
+while (number <= 10){
+    console.log(number)
+    number =number + 2;
 }
