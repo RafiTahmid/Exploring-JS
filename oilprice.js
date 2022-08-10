942275c73736fe0b3c0f2597eec6c04d445b7637
RafiTahmid <rafi.tahmid019@gmail.com>
2022-08-11 00:44:41 +0600

    pblm-3 is done

--- oilprice.js
+++ oilprice.js
@@ -1,24 +1,3 @@
-
-// --------> Problem 1 <-------
-
-function radianToDegree(radian){
-    degree = radian * 57.29;
-    return Math.round(degree);
-}
-// radianToDegree(1);
-// console.log(degree);
-
-
-// --------> Problem 2 <-------
-
-function isJavaScriptFile(myFile){
-    console.log(myFile.endsWith('.js'));
-}
-// isJavaScriptFile('index.js');
-
-
-// --------> Problem 3 <-------
-
 function oilPrice(dieselQuantity, petrolQuantity, octaneQuantity){
     const perLitreDiesel = 114;
     const perLitrePetrol = 130;
@@ -34,4 +13,5 @@ function oilPrice(dieselQuantity, petrolQuantity, octaneQuantity){
 
 }
 
-
+oilPrice(1,2,1);
+console.log(subTotal)
